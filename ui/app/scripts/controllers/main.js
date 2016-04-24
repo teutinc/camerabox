0a56addadd76fdb7e4595c61d29a9215080eeaf0
Augustin Peyrard <augustin.peyrard@gmail.com>
2016-04-24 17:53:04 +0200

    init photobooth production module
    Currently this is just the result of the scaffold done by the restx cli.

---
+++ ui/app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('photoboothApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'RESTX',
+      'Twitter Bootstrap',
+      'AngularJS',
+      'Karma'
+    ];
+  });
